27d907fe574688289731c81834e4fc43943e198a
Alexander Barrere <alexander.barrere@nordstrom.com>
2023-07-24 10:16:39 -0700

    Fixed everything, moving to master

--- backend/handlers.go
+++ backend/handlers.go
@@ -6,31 +6,28 @@ import (
 	"net/http"
 )
 
+// login handles the login API endpoint.
 func login(w http.ResponseWriter, r *http.Request) {
 	var user User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Error decoding request: %v", err)
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Error decoding request: %v", err))
 		return
 	}
 
 	// Validate user input
 	if user.Email == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Email address is required")
+		respondWithError(w, http.StatusBadRequest, "Email address is required")
 		return
 	}
 
 	if !isValidEmail(user.Email) {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Invalid email address")
+		respondWithError(w, http.StatusBadRequest, "Invalid email address")
 		return
 	}
 
 	if user.Password == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Password is required")
+		respondWithError(w, http.StatusBadRequest, "Password is required")
 		return
 	}
 
@@ -39,14 +36,12 @@ func login(w http.ResponseWriter, r *http.Request) {
 	var count int
 	err = db.QueryRow(query, user.Email, user.Password).Scan(&count)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Error checking for existing profile: %v", err)
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error checking for existing profile: %v", err))
 		return
 	}
 
 	if count == 0 {
-		w.WriteHeader(http.StatusUnauthorized)
-		fmt.Fprintf(w, "Invalid email address or password")
+		respondWithError(w, http.StatusUnauthorized, "Invalid email address or password")
 		return
 	}
 
@@ -56,31 +51,28 @@ func login(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
+// register handles the register API endpoint.
 func register(w http.ResponseWriter, r *http.Request) {
 	var user User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Error decoding request: %v", err)
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("Error decoding request: %v", err))
 		return
 	}
 
 	// Validate user input
 	if user.Email == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Email address is required")
+		respondWithError(w, http.StatusBadRequest, "Email address is required")
 		return
 	}
 
 	if !isValidEmail(user.Email) {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "Invalid email address")
+		respondWithError(w, http.StatusBadRequest, "Invalid email address")
 		return
 	}
 
 	if user.City == "" || user.State == "" || user.Country == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintf(w, "City, state, and country are required")
+		respondWithError(w, http.StatusBadRequest, "City, state, and country are required")
 		return
 	}
 
@@ -101,14 +93,12 @@ func register(w http.ResponseWriter, r *http.Request) {
 	var count int
 	err = db.QueryRow(query, user.Email).Scan(&count)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Error checking for existing profile: %v", err)
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error checking for existing profile: %v", err))
 		return
 	}
 
 	if count > 0 {
-		w.WriteHeader(http.StatusConflict)
-		fmt.Fprintf(w, "A profile with that email address already exists")
+		respondWithError(w, http.StatusConflict, "A profile with that email address already exists")
 		return
 	}
 
@@ -116,8 +106,7 @@ func register(w http.ResponseWriter, r *http.Request) {
 	query = "INSERT INTO profile (first_name, last_name, email, password, birth_date, birth_time, city, state, country, latitude, longitude) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING id"
 	err = db.QueryRow(query, user.FirstName, user.LastName, user.Email, user.Password, user.BirthDate, user.BirthTime, user.City, user.State, user.Country, latitudeStr, longitudeStr).Scan(&user.ID)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Error inserting data: %v", err)
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error inserting data: %v", err))
 		return
 	}
 
@@ -128,4 +117,13 @@ func register(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(user)
 	return
+}
+
+// respondWithError writes an error message to the response.
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	w.WriteHeader(code)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"message": message,
+	})
 }
